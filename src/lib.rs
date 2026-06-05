e42cf03cc5b1dc4360f8d40fc76b68dd46e821c7
velocibet <dev.velocibet@gmail.com>
2026-06-05 09:51:35 +0900

    feat: add CPU Exceptions

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![feature(abi_x86_interrupt)]
+
+pub mod interrupts;
+pub mod vga_buffer;
+
+pub fn init() {
+    interrupts::init_idt();
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+#![feature(abi_x86_interrupt)] 
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+mod interrupts;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    interrupts::init_idt();
+    x86_64::instructions::interrupts::int3(); 
+
+    println!("It did not crash!");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
